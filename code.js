425299eda0fef86a05d0772338aba49a9518e3d6
KevinHPark <kevinhyungsungpark@gmail.com>
2021-08-11 02:46:13 -0400

    V.1

---
+++ code.js
@@ -0,0 +1,67 @@
+// create Phaser.Game object named "game"
+var game = new Phaser.Game(1080, 720, Phaser.AUTO, 'superMario',
+{ preload: preload, create: create, update: update });
+
+// declare global variables for game
+var player, arrowKeys, sky, mountain;
+
+// preload game assets - runs once at start
+function preload() {
+   game.load.spritesheet("mario", "assets/images/mario.png", 32, 48);
+   game.load.image("sky", "assets/images/sky.png");
+   game.load.image("mountain", "assets/images/mountain.png");
+}   
+
+// create game world - runs once after "preload" finished
+function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    player = game.add.sprite(25, 300, "mario");
+    player.anchor.set(0.5, 0.5);
+    game.physics.arcade.enable(player);
+    player.body.gravity.y = 600;
+    player.body.collideWorldBounds = true;
+    
+    //Background
+    sky = game.add.tileSprite(0, 0, 1000, 600, 'sky');
+    mountain = game.add.tileSprite(0, 0, 1000, 600, 'mountain');
+
+    //camera
+    game.world.setBounds(0, 0, 5000, 600);
+    game.camera.follow(player);
+    game.add.text(1000, 300, '1000px', { fill: 'white' });    
+    game.add.text(2000, 300, '2000px', { fill: 'white' });    
+    game.add.text(3000, 300, '3000px', { fill: 'white' });    
+    game.add.text(4000, 300, '4000px', { fill: 'white' });
+
+    //input
+    arrowKey = game.input.keyboard.createCursorKeys();
+
+    //animation
+    player.animations.add('left', [0, 1, 2, 3], 10, true);
+    player.animations.add('right', [5, 6, 7, 8], 10, true);
+    player.animations.add('turn', [4], 20, true);
+}
+
+// update gameplay - runs in continuous loop after "create" finished
+function update() {
+    if (arrowKey.right.isDown) {
+        player.body.velocity.x = 200;
+        player.animations.play('right');
+    }
+    else if (arrowKey.left.isDown) {
+        player.body.velocity.x = -200;
+        player.animations.play('left');
+    }
+    else {
+        player.body.velocity.x = 0;
+        player.animations.play('turn');
+    }
+
+    if (arrowKey.up.isDown) {
+        player.body.velocity.y = -400;
+    }
+    // player.animations.stop();
+    // player.frame = 1;
+}
+
+// add custom functions (for collisions, etc.)
