457c84c83963ae75241ff959b3b875104c4cd3b5
KevinHPark <kevinhyungsungpark@gmail.com>
2021-08-11 04:42:48 -0400

    Resolution test

--- code.js
+++ code.js
@@ -1,4 +1,4 @@
-var game = new Phaser.Game(1080, 720, Phaser.AUTO, 'superMario', { preload: preload, create: create, update: update });
+var game = new Phaser.Game(1000, 580, Phaser.AUTO, 'superMario', { preload: preload, create: create, update: update });
 
 var player, arrowKeys, sky, mountain, floor;
 
